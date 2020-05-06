7d577682788a6b4126b71ea7f0d712876dce47ff
Corey Norton <fatcatcooldude@aol.com>
2020-05-06 15:18:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(getsto_i)
+def input_to_index(user_input)
+  user_input = gets.to_i
   puts input_to_index
 end
+
 input_to_index(4)
