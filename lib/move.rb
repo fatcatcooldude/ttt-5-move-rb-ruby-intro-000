d20a9e88b1e0f27e7d0f15fecae511cc27f1ccee
Corey Norton <fatcatcooldude@aol.com>
2020-05-06 15:37:08 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,10 +5,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 user_input = gets.strip
 def input_to_index(user_input)
-
-  puts input_to_index
 end
 
 input_to_index(4)
